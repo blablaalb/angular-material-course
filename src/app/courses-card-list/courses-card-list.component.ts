2751305da41610923498721bb034e8bef6ec1bce
Robert Gazyan <b.b.angel.199x@gmail.com>
2022-05-27 22:33:11 +0400

    05 - Responsive Design With Angular Material finished

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -3,6 +3,7 @@ import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
 import { filter } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
     selector: 'courses-card-list',
@@ -14,11 +15,41 @@ export class CoursesCardListComponent implements OnInit {
     @Input()
     courses: Course[];
 
-    constructor(private dialog: MatDialog) {
+    cols = 3;
+    rowHeight = '500px';
+    handsetPortrait = false;
+
+    constructor(private dialog: MatDialog, private responsive: BreakpointObserver) {
     }
 
     ngOnInit() {
+        this.responsive.observe([
+            Breakpoints.TabletPortrait,
+            Breakpoints.TabletLandscape,
+            Breakpoints.HandsetPortrait,
+            Breakpoints.HandsetLandscape
+        ]).subscribe(result => {
+            this.cols = 3;
+            this.rowHeight = "500px";
+            this.handsetPortrait = false;
 
+            const breakpoints = result.breakpoints;
+            if (breakpoints[Breakpoints.TabletPortrait]) {
+                this.cols = 1;
+            }
+            else if (breakpoints[Breakpoints.HandsetPortrait]) {
+                this.cols = 1;
+                this.rowHeight = "430px";
+                this.handsetPortrait = true;
+            }
+            else if (breakpoints[Breakpoints.HandsetLandscape]) {
+                this.cols = 1;
+            }
+            else if (breakpoints[Breakpoints.TabletLandscape]) {
+                this.cols = 2;
+            }
+        });
+        console.log(this.cols);
     }
 
     editCourse(course: Course) {
@@ -26,7 +57,7 @@ export class CoursesCardListComponent implements OnInit {
         openEditCourseDialog(this.dialog, course).pipe(
             filter(val => !val)
         ).subscribe(
-            val => console.log("new course value ",val)
+            val => console.log("new course value ", val)
         );
     }
 
